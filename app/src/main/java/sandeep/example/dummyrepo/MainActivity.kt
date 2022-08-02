e7e033ecb8e70a1e981b82fdd37fe0680f9aeb72
Ayan Malik <64230492+malik0011@users.noreply.github.com>
2022-08-02 16:43:49 +0530

    Feature1 (#5)
    * added a Log
    
    * added Toast msg
    
    Co-authored-by: sandeep0614 <sandeepyadav936947@gmail.com>

--- app/src/main/java/sandeep/example/dummyrepo/MainActivity.kt
+++ app/src/main/java/sandeep/example/dummyrepo/MainActivity.kt
@@ -2,11 +2,23 @@ package sandeep.example.dummyrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val a =0;
+        val b=0;
+        var c=a+b;
+        Log.d(TAG,"c=$c")
+        val btn=findViewById<Button>(R.id.button)
+        btn.setOnClickListener{
+            Toast.makeText(this,"This is a toast message",Toast.LENGTH_SHORT).show()
+        }
     }
 }
