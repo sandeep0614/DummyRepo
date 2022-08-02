4662fd193a88a17e4baee23871869f518bcf829f
sandeep0614 <sandeepyadav936947@gmail.com>
2022-08-02 16:32:08 +0530

    added a Log

--- app/src/main/java/sandeep/example/dummyrepo/MainActivity.kt
+++ app/src/main/java/sandeep/example/dummyrepo/MainActivity.kt
@@ -2,11 +2,17 @@ package sandeep.example.dummyrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val a =0;
+        val b=0;
+        var c=a+b;
+        Log.d(TAG,"c=$c")
     }
 }
